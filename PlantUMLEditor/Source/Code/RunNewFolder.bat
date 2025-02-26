dfcd9cccab2104615808687dde98fc54d41f29fd
Dick Maley <dickmaley@advdelphisys.com>
2025-02-26 15:24:19 -0500

    Delete PlantUMLEditor/Source/Code/RunNewFolder.bat

--- PlantUMLEditor/Source/Code/RunNewFolder.bat
+++
@@ -1,3 +0,0 @@
-rem "S:\Documents\Delphi\Projects\Utilities\NewFolder\NewFolder.exe" "S:\Documents\Delphi\Projects\Utilities\NewFolder\"
-"S:\Documents\Delphi\Projects\Utilities\NewFolder\NewFolder.exe" "%1"
-pause
